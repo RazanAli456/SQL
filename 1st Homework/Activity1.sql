2c10cb588a40935bc4494ff2473228310d62fda2
RazanAli456 <sultanaiqbal88@gmail.com>
2026-03-19 17:24:42 +0500

    COMMIT

--- 1st Homework/Activity1.sql
+++ 1st Homework/Activity1.sql
@@ -1,5 +1,10 @@
-CREATE TABLE IF NOT EXISTS Tarun(
-EMPOLYEE TEXT
-NAME TEXT
-
+CREATE TABLE IF NOT EXISTS TARUN(
+Empolyee_ID TEXT,
+Name TEXT,
+city TEXT
 );
+INSERT INTO TARUN(Empolyee_ID,NAME,city)
+VALUES ("1","Razan","Lahore"),
+("2","Ali","Karachi"),
+("3","Rayyan","Islmabad");
+SELECT * FROM TARUN;
